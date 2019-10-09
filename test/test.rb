7953490eb98440b5ec1e227d9d7b413cf362f651
Marta Arenas Martínez <martilla311@gmail.com>
2019-10-09 20:12:49 +0200

    Creación de tests closes #5

---
+++ test/test.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require  'test/unit'
+require "json"
+
+require_relative  '../src/ElementoMM'
+
+class TestMyMedia <  Test::Unit::TestCase
+
+	def setup 
+		file = File.read('../src/elemento.json')
+		elem = JSON.parse(file)
+		
+		@elemento = ElementoMM.new(elem['nombre'], elem['tipo'], elem['valoracion'], elem['acceso'], elem['id'])
+		
+	end
+
+	def test_valoracion
+		if @elemento.valoracion < 0 || @elemento.valoracion > 10
+			estado = "incorrecto"
+		else 
+			estado = "correcto"
+		end
+		assert_equal(estado, "correcto", "Valoracion correcta")
+	end
+	
+	def test_tipo
+		if @elemento.tipo =="Serie" || @elemento.tipo == "Película" || @elemento.tipo == "Libro" || @elemento.tipo == "Canción"
+			estado = "correcto"
+		else 
+			estado = "incorrecto"
+		end
+		assert_equal(estado, "correcto", "Tipo correcto")
+	end
+end
